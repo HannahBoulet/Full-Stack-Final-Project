e0356eee987a9d19bf17dcd627ab66fadce2fb2d
HannahBoulet <Hannah.j.boulet@gmail.com>
2024-05-05 12:22:37 -0400

    DONE FINALLY

--- app.ts
+++ app.ts
@@ -20,6 +20,18 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(function (req: Request, res: Response, next: NextFunction) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+})
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
